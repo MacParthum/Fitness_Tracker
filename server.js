7daad12a5982be8243c9da925b6d2b59afeca382
Mac Parthum <macparthum@gmail.com>
2020-09-23 16:44:30 -0400

    Added morgan and path npms

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000
 
@@ -17,6 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
+
+// need get routes for html
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
