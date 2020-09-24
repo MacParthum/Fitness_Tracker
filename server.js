6ff1da4e40b353b795c88db5759e3652f517ca45
Mac Parthum <macparthum@gmail.com>
2020-09-24 17:55:32 -0400

    added app use morgan tiny and removed const require path on server.js since its only needed on the html controller.js

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
 const express = require("express");
-const logger = require("morgan");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
+
 
 const PORT = process.env.PORT || 3000;
 
-const workout = require("./models");
-
 const app = express();
 
-app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(morgan("tiny"))
 app.use(express.static("public"));
 
-app.use(require("./controller/html-controller.js"));
-app.use(require("./controller/api-controller.js"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true, 
-
-useNewUrlParser: true;
-useUnifiedTopology: true,
-useCreateIndex: true,
-useFindAndModify: false,
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+{
+    
+    useNewUrlParser:    true,
+    useUnifiedTopology: true,
+    useCreateIndex:     true,
+    useFindAndModify:   false,
+    
 });
 
-app.use(require(""))
+app.use(require("./controller/html-controller"));
+app.use(require("./controller/api-controller"));
+
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
